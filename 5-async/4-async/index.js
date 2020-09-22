019c19d800f19591abeeae148fb9506f585e5212
Tingzi123 <1044373556@qq.com>
2020-09-22 09:55:00 +0800

    [chenting]:fix async fetch and await

--- 5-async/4-async/index.js
+++ 5-async/4-async/index.js
@@ -3,8 +3,9 @@ async function fetchData(url) {
   // TODO 24: 通过await/async实现异步请求
   // end -->
   const result = await fetch(url);
-  const data = result.json();
+  const data = await result.json();
   console.log(data);
+  document.writeln(data.name);
 }
 
 const URL = 'http://localhost:3000/api';
